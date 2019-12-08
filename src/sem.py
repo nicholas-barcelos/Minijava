91fbd7a6befd52c24f29d786bf80bb913cc9d9ee
Nicholas Campos Barcelos <nicholascb@id.uff.br>
2019-12-08 19:59:24 -0300

    Verifica se valor bate com parâmetro do método.

--- src/sem.py
+++ src/sem.py
@@ -21,14 +21,36 @@ class Symbol:
 
 class Semantic:
     def __init__(self):
-        self.stab = dict()
+        self.stab = None
+        self.calls = None
         self.currClass = None
         self.currMethod = None
 
     def make_table(self,current_node):
+        self.stab = dict()
+        self.calls = dict()
         self.currClass = None
         self.currMethod = None
         self.table_step(current_node)
+        self.verify_calls()
+
+    def verify_calls(self):
+        for classe in self.calls.keys():
+            for metodo in self.calls[classe].keys():
+                for chamada in self.calls[classe][metodo]:
+                    mparams = self.stab[classe][metodo].params
+                    i = 0
+                    for sm in mparams.values():
+                        if chamada[i] is not None:
+                            if chamada[i] != sm.vtype:
+                                raise Exception(
+                                    f"Erro de semântica: Variável na chamada do "
+                                    f"método \'{metodo}\' de \'{classe}\',"
+                                    f"\'{chamada[i]}\' não é \'{sm.vtype}\'."
+                                )
+                        i += 1
+                    # print("metodo params:",mparams)
+                    # print("chamada ",chamada)
 
     def table_step(self,current_node):
         if(isinstance(current_node,nd.Node)):
@@ -107,9 +129,9 @@ class Semantic:
                 vname = current_node.leaf[0].lower()
 
                 if metodo.scopeVars is None:
-                    raise Exception(f"Atribuição de variavel \'{vname}\' inexistente.")
+                    raise Exception(f"Erro de semântica: Atribuição de variavel \'{vname}\' inexistente.")
                 elif vname not in metodo.scopeVars:
-                    raise Exception(f"Atribuição de variavel \'{vname}\' inexistente.")
+                    raise Exception(f"Erro de semântica: Atribuição de variavel \'{vname}\' inexistente.")
                 elif metodo.scopeVars[vname].vtype == 'array':
                     child = current_node.children[0]
                     while len(child.children) > 0:
@@ -137,9 +159,9 @@ class Semantic:
                 vname = current_node.leaf[0].lower()
 
                 if metodo.scopeVars is None:
-                    raise Exception(f"Atribuição de variavel \'{vname}\' inexistente.")
+                    raise Exception(f"Erro de semântica: Atribuição de variavel \'{vname}\' inexistente.")
                 elif vname not in metodo.scopeVars:
-                    raise Exception(f"Atribuição de variavel \'{vname}\' inexistente.")
+                    raise Exception(f"Erro de semântica: Atribuição de variavel \'{vname}\' inexistente.")
                 elif metodo.scopeVars[vname].vtype == 'array':
                     child = current_node.children[0]
                     while len(child.children) > 0:
@@ -149,8 +171,84 @@ class Semantic:
                     if vname in metodo.scopeVars:
                         if (metodo.scopeVars[vname].len is None
                             or metodo.scopeVars[vname].len <= pos):
-                            raise Exception(f"Atribuição de indíce fora de alcance da variavel \'{vname}\': {pos}.")
+                            raise Exception(f"Erro de semântica: Atribuição de indíce fora de alcance da variavel \'{vname}\': {pos}.")
             
+            # Verifica chamada de método
+            elif(current_node.type == 'pexp' 
+                and len(current_node.leaf) > 3
+                and current_node.leaf[0] == "."
+                ):
+                cname = ""
+                mname = ""
+                # new Classe().metodo()
+                if len(current_node.children[0].leaf) > 1:
+                    cname = current_node.children[0].leaf[1].lower()
+                    mname = current_node.leaf[1].lower()
+                # this.metodo()
+                else:
+                    cname = self.currClass
+                    mname = self.currMethod
+
+                # print(
+                #     f"pexp:\n"
+                #     f"  children:{current_node.children}\n"
+                #     f"  leaf:{current_node.leaf}\n"
+                #     f"  cname:{cname}\n"
+                #     f"  mname:{mname}\n"
+                # )
+
+                # verifica o tipo dos operadores desde que não sejam variáveis
+                optexps = current_node.children[1]
+                argtypes = []
+                val = None
+                tp = None
+                if len(optexps.children) > 0:
+                    exp = optexps.children[0].children[0]
+                    while len(exp.children) > 0:
+                        exp = exp.children[0]
+                    try:
+                        val = int(exp.leaf[0])
+                        tp = "int"
+                    except ValueError:
+                        val = exp.leaf[0]
+                        if val.lower() == "true" or val.lower() == "false":
+                            val = "boolean"
+                        else:
+                            tp = None
+                    argtypes.append(tp)
+
+                    # continua operando sobre loopvirgulaexp
+                    loopv = optexps.children[0].children[1]
+                    while len(loopv.children) > 1:
+                        loopvexp = loopv.children[0]
+                        while len(loopvexp.children) > 0:
+                            loopvexp = loopvexp.children[0]
+                        loopv = loopv.children[1]
+
+                        try:
+                            val = int(loopvexp.leaf[0])
+                            tp = "int"
+                        except ValueError:
+                            val = loopvexp.leaf[0]
+                            if val.lower() == "true" or val.lower() == "false":
+                                tp = "boolean"
+                            else:
+                                tp = None
+                        argtypes.append(tp)
+
+                # Adiciona o array de tipos da chamada
+                if cname in self.calls:
+                    classe = self.calls[cname]
+                    if mname in classe:
+                        self.calls[cname][mname].append(argtypes)
+                    else:
+                        classe[mname] = []
+                else:
+                    self.calls[cname] = dict()
+                    self.calls[cname][mname] = []
+                    self.calls[cname][mname].append(argtypes)
+                # print("argtypes",argtypes)
+
             # prossegue a montagem
             for child in current_node.children:            
                 self.table_step(child)
