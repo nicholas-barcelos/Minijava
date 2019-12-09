ae03f0e36f3e74d772919b0578a4fbb90b9c4443
Nicholas Campos Barcelos <nicholascb@id.uff.br>
2019-12-08 21:30:28 -0300

    Add exemplos da analise semantica.

--- out.asm
+++ out.asm
@@ -1,24 +1,16 @@
 .data
 newline: .asciiz "\n"
-aexp_compute_num_aux: .word 0
+fac_computefac_num_aux: .word 0
 .text
 exe_main:
   #sout
-exe_aexp_compute1:
+exe_fac_computefac1:
 sw $fp, 0($sp)
 addiu $sp, $sp, -4
-li $a0, 3
+li $a0, 10
 sw $a0, 0($sp)
 addiu $sp, $sp, -4
-li $a0, 2
-sw $a0, 0($sp)
-addiu $sp, $sp, -4
-li $a0, 1
-
-neg $a0 $a0
-sw $a0, 0($sp)
-addiu $sp, $sp, -4
-jal def_aexp_compute
+jal def_fac_computefac
 li $v0, 1 # especifica o servico de print int
 syscall # printa o $a0
 li $v0, 4 # especifica o servico de print string
@@ -27,8 +19,8 @@ syscall # printa o newline
 # eo_sout
   b end_program
 
-class_aexp:
-def_aexp_compute:
+class_fac:
+def_fac_computefac:
 move $fp, $sp
 sw $ra, 0($sp)
 addiu $sp, $sp, -4
@@ -37,7 +29,7 @@ lw $a0, 4($fp) # Pega arg do metodo
 
 sw $a0, 0($sp)
 addiu $sp, $sp, -4
-li $a0, 0
+li $a0, 1
 
 lw $t1, 4($sp)
 slt $a0, $t1, $a0
@@ -49,78 +41,38 @@ bne $a0, $zero, true0
 lw $a0, 4($fp) # Pega arg do metodo
 sw $a0, 0($sp)
 addiu $sp, $sp, -4
-lw $a0, 12($fp) # Pega arg do metodo
-lw $t1, 4($sp)
-mul $a0, $t1, $a0
-addiu $sp, $sp, 4
-sw $a0, 0($sp)
+exe_fac_computefac2:
+sw $fp, 0($sp)
 addiu $sp, $sp, -4
-lw $a0, 8($fp) # Pega arg do metodo
-lw $t1, 4($sp)
-add $a0, $t1, $a0
-addiu $sp, $sp, 4
-# Computou a expressao
-la $t1, aexp_compute_num_aux
-sw $a0, 0($t1) #Atribuiu a variavel
-
-b eo_true0
-true0:
 lw $a0, 4($fp) # Pega arg do metodo
-sw $a0, 0($sp)
-addiu $sp, $sp, -4
-lw $a0, 8($fp) # Pega arg do metodo
 sw $a0, 0($sp)
 addiu $sp, $sp, -4
 li $a0, 1
 lw $t1, 4($sp)
 sub $a0, $t1, $a0
 addiu $sp, $sp, 4
-lw $t1, 4($sp)
-mul $a0, $t1, $a0
-addiu $sp, $sp, 4
 sw $a0, 0($sp)
 addiu $sp, $sp, -4
-lw $a0, 12($fp) # Pega arg do metodo
+jal def_fac_computefac
 lw $t1, 4($sp)
 mul $a0, $t1, $a0
 addiu $sp, $sp, 4
 # Computou a expressao
-la $t1, aexp_compute_num_aux
+la $t1, fac_computefac_num_aux
+sw $a0, 0($t1) #Atribuiu a variavel
+
+b eo_true0
+true0:
+li $a0, 1
+# Computou a expressao
+la $t1, fac_computefac_num_aux
 sw $a0, 0($t1) #Atribuiu a variavel
 
 eo_true0:
 addiu $sp, $sp, 4
 # retorno
-la $t1, aexp_compute_num_aux
+la $t1, fac_computefac_num_aux
 lw $a0, 0($t1) # Acessa variavel
-# prepara saida
-lw $ra, 4($sp)
-addiu $sp, $sp, 20
-lw $fp, 0($sp)
-jr $ra
-
-def_aexp_negate:
-move $fp, $sp
-sw $ra, 0($sp)
-addiu $sp, $sp, -4
-# cmds do metodo
-#sout
-lw $a0, 4($fp) # Pega arg do metodo
-li $v0, 1 # especifica o servico de print int
-syscall # printa o $a0
-li $v0, 4 # especifica o servico de print string
-la $a0, newline
-syscall # printa o newline
-# eo_sout
-# retorno
-lw $a0, 4($fp) # Pega arg do metodo
-
-beq $a0 $zero zerocheck_1
-li $a0 0
-b eo_zerocheck_1
-zerocheck_1:
-li $a0 1
-eo_zerocheck_1:
 # prepara saida
 lw $ra, 4($sp)
 addiu $sp, $sp, 12
